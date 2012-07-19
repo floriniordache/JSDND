365ca550554d86e94598ab7bf08987ccf957c83a
Florin Iordache <florin.iordache@gmail.com>
2012-07-20 01:16:24 +0300

    Continued implemeting the dragging functionality

--- includes/dragNDrop.js
+++ includes/dragNDrop.js
@@ -1,38 +1,107 @@
 var dragSources = [];
 
+var $draggedElement = undefined;
+
+var mouseStartDragX = 0;
+var mouseStartDragY = 0;
+
+var dragElementOffsetX = 0;
+var dragElementOffsetY = 0;
+
 /*
  * Initializes a DOM element to be a drag source
  * 
  *  Method will iterate over all the element's children and inject the needed mouse event
- *   handlers so that the children will be dragable
+ *   handlers so that the children will be draggable
  */
 function initializeDragSource(elementId) {
 	if($("#"+elementId).length > 0) {
 		dragSources[dragSources.length] = $("#"+elementId);
 		$("#"+elementId).children().mouseover(dragMouseOver);
 		$("#"+elementId).children().mouseout(dragMouseOut);
+		$("#"+elementId).children().mousedown(startDragging);
 	}
 	else {
 		alert( "Could not initialize element with id " + elementId + " as a drag source! " );
 	}
 }
 
+/*
+ * Highlights a draggable element when the mouse hovers over it
+ */
 function dragMouseOver(e) {
 	//only the immediate children of the source are draggable, need to find it based on the actual mouse over target
-	$dragSource = getDragSource($(e.target));
-	if( $dragSource != undefined ) {
-		$dragSource.css("border", "1px solid #FF0000");
+	$dragableElement = getDragableElement($(e.target));
+	if( $dragableElement != undefined ) {
+		$dragableElement.css("border", "1px solid #FF0000");
+		$dragableElement.css({'cursor':'default'});
 	}
 }
 
+/*
+ * Removes highlightling for the draggable element when the mouse moves away from it
+ */
 function dragMouseOut(e) {
-	$dragSource = getDragSource($(e.target));
-	if( $dragSource != undefined ) {
-		$dragSource.css("border", "0");
+	$dragableElement = getDragableElement($(e.target));
+	if( $dragableElement != undefined ) {
+		$dragableElement.css("border", "0");
 	}
 }
 
-function getDragSource($element) {
+/*
+ * Starts dragging the selected element
+ */
+function startDragging(e) {
+	$dragableElement = getDragableElement($(e.target));
+	if( $dragableElement != undefined ) {
+		//set the current draggable element, which we'll use in the mouse move handler
+		$draggedElement = $dragableElement;
+		$dragableElement.css("border", "1px solid #00FF00");
+		$dragableElement.css("position", "absolute");
+		
+		//get the current mouse position
+		mouseStartDragX = e.clientX;
+		mouseStartDragY = e.clientY;
+		
+		dragElementOffsetX = $dragableElement.offset().left;
+		dragElementOffsetY = $dragableElement.offset().top;
+		
+		//change the z index so we can drag the element
+		$dragableElement.css("zIndex", "1000px");
+		
+		//initialize the handlers on the document element
+		$(document).mousemove(drag);
+		$(document).mouseup(finishDrag);
+		
+		// prevent text selection in IE
+        $(document).selectstart(function () { return false; });
+	}
+}
+
+/*
+ * The actual drag method
+ */
+function drag(e) {
+	if($draggedElement != undefined) {
+		$draggedElement.css("left",(dragElementOffsetX + e.clientX - mouseStartDragX) + 'px');
+		$draggedElement.css("top",(dragElementOffsetY + e.clientY - mouseStartDragY) + 'px');
+	}
+}
+
+function finishDrag(e) {
+	if($draggedElement != undefined) {
+		//remove the handlers for the document element
+		$(document).unbind("mousemove");
+		$(document).unbind("mouseup");
+		$(document).unbind("selectstart");
+		
+		$draggedElement.css({'cursor':'default'});
+		
+		$draggedElement = undefined;
+	}	
+}
+
+function getDragableElement($element) {
 	$parents = $element.parents();
 	
 	for( var parentIdx = 0 ; parentIdx < $parents.length ; parentIdx ++ ) {
